60b04f904063e6f3cd4e427183fd8df79f1a9f02
Leonardo Augusto <93408782+Auggustto@users.noreply.github.com>
2023-09-14 13:40:59 -0300

    Trabalhando com operadores

---
+++ 7 Operadores aritméticos/index.py
@@ -0,0 +1,23 @@
+# Soma
+produto1 = 10
+produto2 = 20
+
+print(produto1 + produto2)
+
+# Subtração
+print(produto1 - produto2)
+
+# Divisão
+print(produto1 / produto2)
+
+# Divisão inteira
+print(produto1 // produto2)
+
+# Multiplicação
+print(produto1 * produto2)
+
+# Modulo
+print(produto1 % produto2)
+
+# Exponenciação
+print(produto1 ** produto2)

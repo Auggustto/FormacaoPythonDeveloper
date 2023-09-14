a92b2b1727a9438e5af131d38409163d1e3e918f
Leonardo Augusto <93408782+Auggustto@users.noreply.github.com>
2023-09-14 10:23:50 -0300

    Usando tipo constantes e vareaveis

---
+++ 4 Vareaveis e constantes/index.py
@@ -0,0 +1,16 @@
+# Vareaveis
+nome = "Leonardo"
+idade = 25
+
+nome = "Livia"
+idade = 21
+
+print(nome, idade)
+
+# Dlecarando na mesma linha 
+nome, idade = "Leonardo", 25
+
+print(nome, idade)
+
+# Constantes
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]

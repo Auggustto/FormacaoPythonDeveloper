c71b663def945dbe376109e23c60af66d7708ba1
Leonardo Augusto <93408782+Auggustto@users.noreply.github.com>
2023-09-14 10:55:14 -0300

    Trabalhando com funções de entrada e saida (input e print)

---
+++ 6 Funções de entrada e saída/index.py
@@ -0,0 +1,9 @@
+nome = input("Informe o seu nome:")
+idade = input("Informe a sua idade:")
+# print(nome, idade)
+
+# Usando end
+print(nome, idade, end="...\n")
+
+# Usando sep
+print(nome, idade, sep="#")
